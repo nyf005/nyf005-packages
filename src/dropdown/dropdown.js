90735ef44326a8b9875bf2c90a43fea022b33f2a
Fabrice N'DRI <nyf005@gmail.com>
2022-08-18 03:19:13 +0000

    Restructure project folder and create dopdown function

---
+++ src/dropdown/dropdown.js
@@ -0,0 +1,42 @@
+import "./css/style.css";
+import "./dropdown.css";
+import chevronDown from "../assets/icons/chevron-down.svg";
+
+export default function Dropdown(elementName, items) {
+  const dropdown = document.createElement("div");
+  dropdown.className = "dropdown";
+
+  const element = document.createElement("div");
+  element.classList.add("element");
+
+  const p = document.createElement("p");
+  p.textContent = elementName;
+
+  const icon = document.createElement("img");
+  icon.src = chevronDown;
+  icon.setAttribute("alt", "chevron-down");
+  icon.classList.add("element-icon");
+
+  element.append(p, icon);
+
+  const elementItems = document.createElement("ul");
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    let elementItem = document.createElement("a");
+    elementItem.textContent = item.name;
+    elementItem.href = item.to;
+    li.appendChild(elementItem);
+    elementItems.appendChild(li);
+  });
+
+  dropdown.append(element, elementItems);
+
+  elementItems.addEventListener("mouseover", () => {
+    element.classList.add("active");
+  });
+
+  elementItems.addEventListener("mouseout", () => {
+    element.classList.remove("active");
+  });
+  return dropdown;
+}
