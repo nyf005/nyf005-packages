2edca55bac1b7132b18e4dc2160ffb57b2908246
Fabrice N'DRI <nyf005@gmail.com>
2022-08-19 00:19:04 +0000

    Create carousel dot

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -6,13 +6,13 @@ export default function Carousel(images) {
   const imgContainer = document.createElement("div");
   imgContainer.id = "img-container";
 
-  const leftBtn = document.createElement("div");
+  const leftBtn = document.createElement("button");
   leftBtn.id = "left";
   const chevronLeft = document.createElement("img");
   chevronLeft.src = leftChevron;
   leftBtn.appendChild(chevronLeft);
 
-  const rightBtn = document.createElement("div");
+  const rightBtn = document.createElement("button");
   rightBtn.id = "right";
   const chevronRight = document.createElement("img");
   chevronRight.src = rightChevron;
@@ -22,9 +22,27 @@ export default function Carousel(images) {
   pictureDiv.id = "picture";
   const picture = document.createElement("img");
   picture.src = images[0];
-  pictureDiv.appendChild(picture);
 
-  imgContainer.append(leftBtn, picture, rightBtn);
+  const dots = document.createElement("div");
+  dots.id = "dots";
+
+  images.forEach((_, index) => {
+    let dot = new Dot(index);
+    console.log(dot);
+    dots.appendChild(dot);
+  });
+
+  pictureDiv.append(picture, dots);
+
+  imgContainer.append(leftBtn, pictureDiv, rightBtn);
 
   return imgContainer;
+}
+
+function Dot(index) {
+  const dot = document.createElement("div");
+  dot.className = "dot";
+  dot.setAttribute("data-img", index);
+
+  return dot;
 }
